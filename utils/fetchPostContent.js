8fc55e687540b854174d3bdb25c63d87f93c7d7d
rupam2001 <rjdasgalexy@gmail.com>
2021-05-18 18:12:19 +0530

    path problem

--- utils/fetchPostContent.js
+++ utils/fetchPostContent.js
@@ -1,6 +1,9 @@
 export function fetchPostContent(postDir) {
     const fs = require('fs');
-    const dirPath = "./public/data/blogs/" + postDir
+    const path = require("path")
+
+
+    const dirPath = path.join(process.cwd(), "./public/data/blogs/") + postDir
     const htmlPage = fs.readFileSync(dirPath + "/index.html", "utf8")
     return htmlPage
 }
